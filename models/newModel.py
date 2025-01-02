053c042af2f460d73bdd74dfa09b413857c5fbbe
Amlan66 <amlan.routray@gmail.com>
2025-01-03 01:57:36 +0530

    fix

--- models/newModel.py
+++ models/newModel.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchsummary import summary
 
 class DilatedNet(nn.Module):
     def __init__(self):
